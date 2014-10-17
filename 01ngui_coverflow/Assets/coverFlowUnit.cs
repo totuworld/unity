bdb81fcacfe785e10ec2c09e7a7489bcd6c45b71
totuworld <totuworld@gmail.com>
2014-10-17 18:11:51 +0900

    add code and resource

---
+++ 01ngui_coverflow/Assets/coverFlowUnit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coverFlowUnit : MonoBehaviour {
+
+	Transform mTrans;
+	UIPanel mPanel;
+	UIWidget mWidget;
+
+	float cellWidth;
+	float downScale;
+
+	void Start()
+	{
+		mTrans = transform;
+		mPanel = mTrans.parent.parent.GetComponent<UIPanel>();
+		mWidget = GetComponent<UIWidget>();
+		
+		cellWidth = 300;
+		downScale = 0.35f;
+	}
+	
+	Vector3 pos, calPos;
+	
+	void FixedUpdate()
+	{
+		calPos = new Vector3(mPanel.clipOffset.x, 0, 0);
+		pos = mTrans.localPosition - calPos;
+		float dist = Mathf.Clamp(Mathf.Abs(pos.x), 0f, cellWidth);
+
+		mWidget.width = System.Convert.ToInt32( ((cellWidth - dist * downScale) / cellWidth) * cellWidth );
+	}
+}

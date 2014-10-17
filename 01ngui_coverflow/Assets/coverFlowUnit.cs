b0699d0cd68943e604d335776f352d4022ea6c47
totuworld <totuworld@gmail.com>
2014-10-17 19:01:36 +0900

    code update

--- 01ngui_coverflow/Assets/coverFlowUnit.cs
+++ 01ngui_coverflow/Assets/coverFlowUnit.cs
@@ -4,10 +4,17 @@ using System.Collections;
 public class coverFlowUnit : MonoBehaviour {
 
 	Transform mTrans;
+	/// <summary>
+	/// 패널의 값을 알아내기 위한 것.
+	/// </summary>
 	UIPanel mPanel;
+	/// <summary>
+	/// UISprite나 UITexture 등이 상속받은 상위 클래스 UIWidget으로 Size를 조절할 때 사용.
+	/// </summary>
 	UIWidget mWidget;
-
+	//unit의 가로 크기.
 	float cellWidth;
+	//작아졌을 때 크기.
 	float downScale;
 
 	void Start()
@@ -15,19 +22,20 @@ public class coverFlowUnit : MonoBehaviour {
 		mTrans = transform;
 		mPanel = mTrans.parent.parent.GetComponent<UIPanel>();
 		mWidget = GetComponent<UIWidget>();
-		
+
+		//초기값 입력.
 		cellWidth = 300;
 		downScale = 0.35f;
 	}
 	
-	Vector3 pos, calPos;
+	float pos, dist;
 	
 	void FixedUpdate()
 	{
-		calPos = new Vector3(mPanel.clipOffset.x, 0, 0);
-		pos = mTrans.localPosition - calPos;
-		float dist = Mathf.Clamp(Mathf.Abs(pos.x), 0f, cellWidth);
-
+		//중심점과 거리가 얼마나 멀어졌는지 확인.
+		pos = mTrans.localPosition.x - mPanel.clipOffset.x;
+		dist = Mathf.Clamp(Mathf.Abs(pos), 0f, cellWidth);
+		//width값을 조절하여 sprite의 size를 조정. 
 		mWidget.width = System.Convert.ToInt32( ((cellWidth - dist * downScale) / cellWidth) * cellWidth );
 	}
 }
